0bfb7f7221bd49473d2fbdc00aaa1dc7c2b03436
dermatologist <github@gulfdoctor.net>
2025-09-20 10:46:13 -0500

    feat: Remove unused import from useBundle hook

--- src/hooks/useBundle.ts
+++ src/hooks/useBundle.ts
@@ -1,7 +1,6 @@
 
 import axios from "axios";
 import { CDSHookRequest } from "../models/request";
-import { input } from "@testing-library/user-event/dist/types/event";
 
 /**
  * Sends a CDS Hook Request where the request.context contains the user's input message.
